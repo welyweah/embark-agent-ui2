6b4d4c123ed5643456bc2c2fd94bdea16f476994
W <91365231+welyweah@users.noreply.github.com>
2025-01-31 09:26:03 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ const initialFacts = [
   "Welcome to fact bubbles, we are here to help you throughout the process so you focus on building your exciting start-up :)",
   "Did you know Embark.Law is one of the most innovative startup laywers in Switzerland?",
   "We will appear at time to explain complex concepts that you might not be familiar with",
-  "Did you know 'Vesting Agreemnts' are needed to make sure each shareholder applies to a number of rules before taking out shares, its important, but dont worry we have excellent suggestions ",
+  "Did you know 'Vesting Agreements' are needed to make sure each shareholder applies to a number of rules before taking out shares, its important, but dont worry we have excellent suggestions ",
 ];
 
 const suggestions = [
